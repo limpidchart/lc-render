63963368b5e4ddbe6b5a9339a4bcbfa5abcde4d7
Andrei Ozerov <andrei.ozerov92@gmail.com>
2021-05-06 21:12:41 +0300

    Add view package and Orientation enum (#13)
    Add package that will contain different chart views.
    
    Add orientation that will be used for views that use shapes that are
    configured by orientation.

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,13 @@ pub use crate::scale::band::BandScale;
 pub use crate::scale::linear::LinearScale;
 pub use crate::scale::{Scale, ScaleKind};
 pub use crate::value::bar::BarsValues;
+pub use crate::view::Orientation;
 
 pub mod color;
 pub mod error;
 pub mod scale;
 pub mod value;
+pub mod view;
 
 pub(crate) mod math;
 pub(crate) mod render;
