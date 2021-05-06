c7c6e99bd6369ac076cbb77d76e6c7554590a047
Andrei Ozerov <andrei.ozerov92@gmail.com>
2021-05-06 21:14:12 +0300

    Add shape backage and Bar shape
    Add package that will contain shapes for different chart views.
    
    Add Bar shape for bar views.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub use crate::color::Color;
 pub use crate::scale::band::BandScale;
 pub use crate::scale::linear::LinearScale;
 pub use crate::scale::{Scale, ScaleKind};
+pub use crate::shape::bar::BarLabelPosition;
 pub use crate::value::bar::BarsValues;
 pub use crate::view::Orientation;
 
@@ -13,3 +14,4 @@ pub mod view;
 
 pub(crate) mod math;
 pub(crate) mod render;
+pub(crate) mod shape;
