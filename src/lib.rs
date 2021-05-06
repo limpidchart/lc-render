659ebeb916e35c8996a02127ac6b82b97cca5a0d
Andrei Ozerov <andrei.ozerov92@gmail.com>
2021-05-06 19:52:52 +0300

    Add value package and BarsValues (#12)
    Add BarsValues to use with bar views.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
+pub use crate::color::Color;
 pub use crate::scale::band::BandScale;
 pub use crate::scale::linear::LinearScale;
 pub use crate::scale::{Scale, ScaleKind};
+pub use crate::value::bar::BarsValues;
 
 pub mod color;
 pub mod error;
 pub mod scale;
+pub mod value;
 
 pub(crate) mod math;
 pub(crate) mod render;
